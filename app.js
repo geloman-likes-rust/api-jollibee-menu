3c52432794573fde7686987988dd6a598e0d06d6
geloman <geloman-likes-rust@users.noreply.github.com>
2023-03-26 05:04:44 +0800

    Create Server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+require("dotenv").config()
+const PORT = process.env.port || 3000
+
+app.use(express.static("./public"))
+app.get("/home", (_, res) => res.status(200).sendFile(`${__dirname}/models/home.json`))
+app.get("/menu", (_, res) => res.status(200).sendFile(`${__dirname}/models/menus.json`))
+app.get("/menu/:item", (req, res) => res.status(200).sendFile(`${__dirname}/models/menu/${req.params.item}.json`))
+
+app.listen(PORT)
+
