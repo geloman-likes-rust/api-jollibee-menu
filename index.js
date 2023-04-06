6eca5a8158aefa618b0b4253517fa23f75cf6036
Gelo <geloman-likes-rust@users.noreply.github.com>
2023-04-07 04:56:45 +0800

    Access-Control-Allow-Origin *

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const app = express();
 require("dotenv").config()
 const PORT = process.env.port || 3000
 
+app.use(cors())
 app.use(express.static(`${__dirname}/public`))
-//app.get("/", (_, res) => res.status(200).sendFile(`${__dirname}/public/index.html`))
 app.get("/home", (_, res) => res.status(200).sendFile(`${__dirname}/models/home.json`))
 app.get("/menu", (_, res) => res.status(200).sendFile(`${__dirname}/models/menus.json`))
 app.get("/menu/:item", (req, res) => res.status(200).sendFile(`${__dirname}/models/menu/${req.params.item}.json`))
